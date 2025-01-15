d7880b95176345ed8d2b384ec2b995f02e8e6d60
muny phalla <lalaphalla@gmail.com>
2025-01-15 17:12:43 +0700

    refactor: change base url

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const baseURL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://todo-ts-backend-x7zo.onrender.com/' // Replace with your actual Vercel URL
-    : 'http://localhost:3000'; // Local development URL
+// const baseURL =
+//   process.env.NODE_ENV === 'production'
+//     ? 'https://todo-ts-backend-x7zo.onrender.com/' // Replace with your actual Vercel URL
+//     : 'http://localhost:3000'; // Local development URL
 
 const axiosInstance = axios.create({
-  baseURL, // Ensure you include 'http://' or 'https://'
+  baseURL: 'https://todo-ts-backend-x7zo.onrender.com/', // Ensure you include 'http://' or 'https://'
   timeout: 10000, // Optional timeout setting in milliseconds
   headers: {
     'Content-Type': 'application/json',
