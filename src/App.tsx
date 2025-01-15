690410b86989f772081a6d89fec8e0c98d0ff663
muny phalla <lalaphalla@gmail.com>
2025-01-15 12:59:09 +0700

    refactor: comment todo page

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { HomeView } from './pages/Home';
 import ResponsiveAppBar from './components/Navbar';
 // import TodoListView from './pages/todo-list/view';
 import { TodoList } from './pages/todo-list';
-import { Todo } from './pages/todo';
 import useTodoHandler from './hooks/use-todo';
+// import { Todo } from './pages/todo';
 
 function App() {
   useTodoHandler();
@@ -14,7 +14,7 @@ function App() {
       <ResponsiveAppBar />
       <Routes>
         <Route path="/" element={<HomeView />} />
-        <Route path="/todo" element={<Todo />} />
+        {/* <Route path="/todo" element={<Todo />} /> */}
         <Route path="/todolist" element={<TodoList />} />
       </Routes>
     </>
